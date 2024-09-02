e4023a81b53d22b233ff16abb6572fb1e2d98303
cbpark-nota <cbpark@nota.ai>
2024-09-02 04:45:49 +0000

    Update setup.py for version 0.1.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ install_requires = Path("requirements.txt").read_text().split('\n')
 
 setup(
     name="netspresso_inference_package",
-    version="0.1.2",
+    version="0.1.4",
     author="NetsPresso",
     author_email="netspresso@nota.ai",
     description="Inference module.",
